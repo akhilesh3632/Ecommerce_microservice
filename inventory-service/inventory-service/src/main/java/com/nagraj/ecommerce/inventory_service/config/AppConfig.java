d8e8dd57c6a28b4d03d08194c1348a7494214389
akhilesh3632 <chamoliakhilesh24@gmail.com>
2026-02-11 17:33:37 +0530

    Added Eureka discovery-service

--- inventory-service/inventory-service/src/main/java/com/nagraj/ecommerce/inventory_service/config/AppConfig.java
+++ inventory-service/inventory-service/src/main/java/com/nagraj/ecommerce/inventory_service/config/AppConfig.java
@@ -4,6 +4,7 @@ package com.nagraj.ecommerce.inventory_service.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class AppConfig {
@@ -11,6 +12,10 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+    @Bean
+    public RestClient restClient(){
+        return RestClient.builder().build();
     }
 
 }
